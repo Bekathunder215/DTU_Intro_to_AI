c92a7783d97521b1bf6e773198f6ba4a18845ba8
Bekathunder215 <johnbekiaris13@gmail.com>
2025-03-05 16:48:42 +0100

    added movement

--- game.py
+++ game.py
@@ -7,4 +7,26 @@ class Game:
         self.grid = np.zeros((GRID_SIZE, GRID_SIZE), dtype=int)
     
     def move(self, direction):
-        pass  # Movement logic will be implemented here
+        def slide_and_merge(row):
+            non_zero = [num for num in row if num != 0]  # Remove zeros
+            new_row = []
+            skip = False
+            for i in range(len(non_zero)):
+                if skip:
+                    skip = False
+                    continue
+                if i < len(non_zero) - 1 and non_zero[i] == non_zero[i + 1]:
+                    new_row.append(non_zero[i] * 2)
+                    skip = True  # Skip the next tile since it's merged
+                else:
+                    new_row.append(non_zero[i])
+            return new_row + [0] * (GRID_SIZE - len(new_row))  # Fill with zeros
+        
+        if direction == "left":
+            self.grid = np.array([slide_and_merge(row) for row in self.grid])
+        elif direction == "right":
+            self.grid = np.array([slide_and_merge(row[::-1])[::-1] for row in self.grid])
+        elif direction == "up":
+            self.grid = np.transpose([slide_and_merge(row) for row in self.grid.T])
+        elif direction == "down":
+            self.grid = np.transpose([slide_and_merge(row[::-1])[::-1] for row in self.grid.T])
